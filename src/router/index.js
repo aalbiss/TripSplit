6cd0ca1263a17eadc6f9c0c0129f95bfa65d9061
aalbiss <albertoambrosi6@gmail.com>
2025-03-18 21:56:27 +0100

    Added routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,27 @@
+import LandingPage from '@/views/LandingPageView.vue'
+import SigupView from '@/views/SigupView.vue'
+import StartView from '@/views/StartView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{}],
+  routes: [
+    {
+      path: '/',
+      name: 'LandingPage',
+      component: LandingPage,
+    },
+    {
+      path: '/signup',
+      name: 'SignupPage',
+      component: SigupView,
+    },
+    {
+      path: '/start',
+      name: 'Start',
+      component: StartView,
+    },
+  ],
 })
 
 export default router
