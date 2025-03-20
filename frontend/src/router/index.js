d190ca024b6f5638ef630bee728796cb578d36aa
aalbiss <121013006+aalbiss@users.noreply.github.com>
2025-03-20 11:21:06 +0000

    Updated routes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import LandingPage from '@/views/LandingPageView.vue'
 import SigupView from '@/views/SigupView.vue'
 import StartView from '@/views/StartView.vue'
+import CreateGroupView from '@/views/CreateGroupView.vue'
+import Prova from '@/views/Prova.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
@@ -20,6 +22,16 @@ const router = createRouter({
       path: '/start',
       name: 'Start',
       component: StartView,
+    },
+    {
+      path: '/createGroup',
+      name: 'CreateGroup',
+      component: CreateGroupView,
+    },
+    {
+      path: '/prova',
+      name: 'Prova',
+      component: Prova,
     },
   ],
 })
